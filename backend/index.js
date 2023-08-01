0db49f1fe36b47a5d062b0b58ebf2634084ac72a
Oleksandr Antonenko <alexandr.antonenko.fr@gmail.com>
2023-08-01 23:20:13 +0300

    TA-4 reorganised project structure

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import jwt from 'jsonwebtoken'
+
+const app = express();
+app.use(express.json())
+app.get('/', (req, res) => {
+  res.send('Hi, I am a Server');
+});
+
+app.post('/auth/login', (req, res) => {
+  const token = jwt.sign({
+    email: req.body.email,
+  }, 'hash2023')
+
+  res.json({
+    success: true,
+    token,
+  })
+})
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err)
+  }
+
+  console.log('Server OK')
+})
