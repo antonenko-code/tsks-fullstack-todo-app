4469ffe7ac526bedeae69014d60c2163fc9a3c20
Serhii Vorobei <s.vorobei.dev@gmail.com>
2023-08-03 12:41:38 +0300

    TA-5 pages has been updated

--- frontend/src/pages/StartPage/StartPage.tsx
+++ frontend/src/pages/StartPage/StartPage.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import styles from './StartPage.module.scss'
-import { MainButton } from '../../components/buttons/MainButton';
-import { RegistrationBtn } from '../../components/buttons/RegButton/RegistrationBtn';
 
 export const StartPage = () => {
   return (
@@ -9,10 +7,6 @@ export const StartPage = () => {
       <div className={styles.block}>
         Hello, I'm Start Page
       </div>
-      <MainButton text={'Get Started'} gradient={true}/>
-      <MainButton text={'Try Demo'} fullwidth={true} />
-      <RegistrationBtn text={'Sing in'} />
-      <RegistrationBtn text={'Sing up'} border={true}/>
     </>
   );
 };
