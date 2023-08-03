ea9960ea90f53c01f7b46017610c9cec3db0b8f2
Serhii Vorobei <s.vorobei.dev@gmail.com>
2023-08-03 09:28:04 +0300

    TA-5 Btm has been created

--- frontend/src/pages/StartPage/StartPage.tsx
+++ frontend/src/pages/StartPage/StartPage.tsx
@@ -1,11 +1,18 @@
-import classNames from 'classnames';
 import React from 'react';
 import styles from './StartPage.module.scss'
+import { MainButton } from '../../components/buttons/MainButton';
+import { RegistrationBtn } from '../../components/buttons/RegButton/RegistrationBtn';
 
 export const StartPage = () => {
   return (
-    <div className={styles.block}>
-      Hello, I'm Start Page
-    </div>
+    <>
+      <div className={styles.block}>
+        Hello, I'm Start Page
+      </div>
+      <MainButton text={'Get Started'} gradient={true}/>
+      <MainButton text={'Try Demo'} fullwidth={true} />
+      <RegistrationBtn text={'Sing in'} />
+      <RegistrationBtn text={'Sing up'} border={true}/>
+    </>
   );
 };
