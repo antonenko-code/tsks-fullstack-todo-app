1211a3746eb69f9ee67fc361ec31c5517ca591f6
Maryna Kucher <marina.kucher8@gmail.com>
2023-08-06 18:04:28 +0300

    TA-6_form components, icon selector have been created

---
+++ frontend/src/shared/FormField/FormField.tsx
@@ -0,0 +1,29 @@
+import React, { FC, ReactNode } from 'react';
+import styles from './FormField.module.scss'
+
+interface Props extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>{
+  children: ReactNode;
+}
+
+export const FormField: FC<Props> = ({children, ...props}) => {
+  return (
+    <div>
+      <label
+        className={styles.block}
+      >
+        {props.title}
+        <div
+          className={styles.inputWrapper}
+        >
+          <input
+            className={styles.input}
+            {...props}
+          >
+          </input>
+          {children}
+        </div>
+      </label>
+    </div>
+
+  );
+};
