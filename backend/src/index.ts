e254355d44f9683d803f5e0b186e3d9f176b8ddc
Oleksandr Antonenko <alexandr.antonenko.fr@gmail.com>
2023-08-09 12:48:14 +0300

    Email sending has been added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,25 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { resolve } from 'path';
 import authRouter from './router/authRouter';
+dotenv.config({path: resolve(__dirname,"../.env")});
+
+const PORT = process.env.PORT || 8080;
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 app.use('/auth', authRouter);
 
 const startServer = async () => {
   try {
-    await mongoose.connect('mongodb://userAdmin:userPassword@localhost:27017/todoapp?authSource=admin')
-    app.listen(8080, () => {
-      console.log(`Server sarted...`)
-    })
-    app.get('/', (req, res) => {
-      res.send('Hello World');
+    await mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`)
+    app.listen(PORT, () => {
+      console.log(`Server started... on PORT = ${PORT}`)
     })
   } catch (e) {
     console.log(e)
