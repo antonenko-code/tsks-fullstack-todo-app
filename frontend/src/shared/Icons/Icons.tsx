99c04a6a0c7617326d6339efd5e6b1acacbfd95e
Maryna Kucher <marina.kucher8@gmail.com>
2023-08-10 18:12:27 +0300

    TA-8_component TaskItem created, CategoryItem has been renamed to CollectionItem, svg 'calendar' has been added to Icons component

--- frontend/src/shared/Icons/Icons.tsx
+++ frontend/src/shared/Icons/Icons.tsx
@@ -31,6 +31,15 @@ export const Icons: React.FC<Props> = ({ name, className }) => {
           <path d="M9 13.5L7.5 16" stroke="#717586" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
       )
+    case 'calendar':
+      return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+          <path d="M8.25 9.75H12M6 9.75H6.00673M9.75 12.75H6M12 12.75H11.9933" stroke="#8C8C9E" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M13.5 1.5V3M4.5 1.5V3" stroke="#8C8C9E" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M1.875 9.1824C1.875 5.91446 1.875 4.28046 2.81409 3.26523C3.75318 2.25 5.26462 2.25 8.2875 2.25H9.7125C12.7354 2.25 14.2469 2.25 15.1859 3.26523C16.125 4.28046 16.125 5.91446 16.125 9.1824V9.5676C16.125 12.8356 16.125 14.4695 15.1859 15.4848C14.2469 16.5 12.7354 16.5 9.7125 16.5H8.2875C5.26462 16.5 3.75318 16.5 2.81409 15.4848C1.875 14.4695 1.875 12.8356 1.875 9.5676V9.1824Z" stroke="#8C8C9E" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M2.25 6H15.75" stroke="#8C8C9E" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      )
     default:
       return null;
   }
