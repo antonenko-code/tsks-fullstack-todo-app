1198cbdbe3ce2a1ac75a54056498b4675aeb1100
Serhii Vorobei <s.vorobei.dev@gmail.com>
2023-08-10 08:53:45 +0300

    TA-15 Banner was added to Startpage

--- frontend/src/pages/StartPage/StartPage.tsx
+++ frontend/src/pages/StartPage/StartPage.tsx
@@ -2,35 +2,37 @@ import React from 'react';
 import styles from './StartPage.module.scss'
 import { MainButton } from '../../components/buttons/MainButton';
 import { PageLayout } from '../../shared/PageLayout';
+import banner from '../../app/assets/images/banner.png';
 
 export const StartPage = () => {
   return (
     <PageLayout>
-      <div>
+      <div className={styles.block}>
         <div>
-          <h1 className={styles.header}>
-            <span className={styles.highlight}>tsks...</span>
-            just tasks
-          </h1>
+          <div>
+            <h1 className={styles.header}>
+              <span className={styles.highlight}>tsks...</span><br/>
+              just tasks
+            </h1>
+          </div>
+
+          <p className={styles.subtitle}>
+            Keep track of the daily tasks in life and get that satisfaction upon completion.
+          </p>
+
+          <div className={styles.buttons}>
+            <MainButton text={'Get Started'} gradient={true} />
+
+            <MainButton text={'Try Demo'} />
+          </div>
         </div>
 
-        <p className={styles.subtitle}>
-          Keep track of the daily tasks in life and get that satisfaction upon completion.
-        </p>
-
-        <div className={styles.buttons}>
-          <MainButton text={'Get Started'} gradient={true} />
-
-          <MainButton text={'Try Demo'} />
+        <div>
+          <img
+            src={banner}
+            alt="Happy girl"
+          />
         </div>
-      </div>
-
-      <div>
-        <img
-          className={styles.banner}
-          src="../../app/assets/images/banner.png"
-          alt="Happy girl"
-        />
       </div>
     </PageLayout>
   );
