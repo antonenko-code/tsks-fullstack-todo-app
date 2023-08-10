5293f7af2f39157a6f091f810f0ead2cbecf3915
Maryna Kucher <marina.kucher8@gmail.com>
2023-08-11 00:48:30 +0300

    TA-16_Footer component has been created, styles is fixed

--- frontend/src/pages/Registration/Registration.tsx
+++ frontend/src/pages/Registration/Registration.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './Registration.module.scss';
+import { PageLayout } from '../../shared/PageLayout';
 
 export const Registration: React.FC = () => {
   return (
-    <div className={styles.block}>
-      <h2>
-        Registration
-      </h2>
-    </div>
+    <PageLayout>
+      <div className={styles.block}>
+        <h2>
+          Registration
+        </h2>
+      </div>
+    </PageLayout>
   );
 };
---
+++ frontend/src/wigets/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Footer.module.scss';
+
+export const Footer = () => {
+  return (
+    <div className={styles.block}>
+      © 2023 | tsks just tasks
+    </div>
+  );
+};
