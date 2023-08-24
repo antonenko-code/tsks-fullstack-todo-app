db41851414f5b13541a3173950b73c4eeb461d1b
Serhii Vorobei <s.vorobei.dev@gmail.com>
2023-08-24 12:57:52 +0300

    TA-36 fixed mistakes

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-// import { SignIn } from './SignIn';
+import { SignIn } from './SignIn';
 import { ForgotPassword } from './ForgotPassword';
 import { PasswordReset } from './PasswordReset';
 import { Registration } from './Registration';
@@ -9,14 +9,12 @@ import { Collections } from './Collections';
 import { Home } from './Home';
 import { Tasks } from './Tasks';
 
-
-
 export const Routing = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
 
-      <Route path='sign-in' element={<Account /> } />
+      <Route path='sign-in' element={<SignIn /> } />
 
       <Route path='forgot-password' element={<ForgotPassword />} />
 
@@ -24,7 +22,7 @@ export const Routing = () => {
 
       <Route path='sign-up' element={<Registration />} />
 
-      {/*<Route path='account' element={<Account />} />*/}
+      <Route path='account' element={<Account />} />
 
       <Route path="collections">
         <Route index element={<Collections />} />
