e843eff58bc12d40dc6c8a44e5a645d15460e4f8
Oleksandr Antonenko <alexandr.antonenko.fr@gmail.com>
2023-08-30 00:56:10 +0300

    Token Authentication Middleware is implemented

--- backend/src/service/tokenService.ts
+++ backend/src/service/tokenService.ts
@@ -12,6 +12,24 @@ class TokenService {
     }
   }
 
+  validateAccessToken(token: string) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET as string);
+      return userData;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  validateRefreshToken(token: string) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET as string);
+      return userData;
+    } catch (e) {
+      return null;
+    }
+  }
+
   async saveToken(userId: string, refreshToken: string) {
     const tokenData = await Token.findOne({user: userId})
 
