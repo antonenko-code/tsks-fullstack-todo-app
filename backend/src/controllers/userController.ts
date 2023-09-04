4e79d35d6c89fdbfb0ea18dadfed4f961876298e
Oleksandr Antonenko <alexandr.antonenko.fr@gmail.com>
2023-09-04 18:29:47 +0300

    [TECH] Request type extend error is fixed

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,16 +1,16 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import userService from '../service/userService';
 import { UserDocument } from '../models/user-model';
 
 declare module 'express' {
-  interface Request {
-    user: UserDocument;
+  export interface Request {
+    user?: UserDocument;
   }
 }
 
 class userController {
   async getUser(req: Request, res: Response, next: NextFunction) {
-    const userId = req.user.id;
+    const userId = req.user?.id;
     const user = await userService.getUser(userId);
 
     return res.json(user);
