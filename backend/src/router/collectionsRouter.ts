f68423224a72ae2d5df7fc460268796a70901d19
Oleksandr Antonenko <alexandr.antonenko.fr@gmail.com>
2023-09-06 18:19:30 +0300

    Endpoint for deleting a collection is implemented

--- backend/src/router/collectionsRouter.ts
+++ backend/src/router/collectionsRouter.ts
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/collections', AuthMiddleware, collectionsController.getCollections);
 router.post('/collections', AuthMiddleware, collectionsController.setCollection);
 router.patch('/collections/:id', AuthMiddleware, collectionsController.updateCollection);
+router.delete('/collections/:id', AuthMiddleware, collectionsController.deleteCollection);
 
 export default router;
