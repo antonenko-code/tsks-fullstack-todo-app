fa3341ddc3859383a45dd62455c3c602a01fda51
Oleksandr Antonenko <alexandr.antonenko.fr@gmail.com>
2023-09-06 16:03:54 +0300

    [TECH] Cors is configured

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,10 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: '*'
+}));
 app.use('/auth', authRouter);
 app.use(userRouter);
 app.use(collectionsRouter);
