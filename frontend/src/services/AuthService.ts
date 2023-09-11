5fbe600f7c36e63cd48956288e62d4801a9ac831
Serhii Vorobei <s.vorobei.dev@gmail.com>
2023-09-11 17:31:38 +0300

    TA-41 Registration function was created

--- frontend/src/services/AuthService.ts
+++ frontend/src/services/AuthService.ts
@@ -3,9 +3,14 @@ import { AxiosResponse } from 'axios';
 import { IBaseResponse } from '../types/response/IBaseResponse';
 import { IAuthData } from '../types/response/IAuthData';
 import { IValidationError } from '../types/response/IValidationError';
+import { RequestRegistrationData } from '../types/request/IRequestRegistrationData';
 
 export class AuthService {
   static async login(email: string, password: string): Promise<AxiosResponse<IBaseResponse<IAuthData, IValidationError[]>>> {
     return $api.post<IBaseResponse<IAuthData, IValidationError[]>>('/auth/login', {email, password})
+  }
+
+  static async registration(data: RequestRegistrationData): Promise<AxiosResponse<IBaseResponse<IAuthData, IValidationError[]>>> {
+    return $api.post<IBaseResponse<IAuthData, IValidationError[]>>('/auth/registration', data)
   }
 }
